3031f14b4313ff02a7100f95ec9c5f3ce3554579
Siko <bsj2234@naver.com>
2024-04-18 19:11:00 +0900

    Fixing EffectSpawn and clening with combat component

--- Assets/06.Sprites/HollowKnight/CoinRock/Destructable.cs
+++ Assets/06.Sprites/HollowKnight/CoinRock/Destructable.cs
@@ -2,37 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
-public interface IDestructible
-{
-    public void Hit();
-    public void Destruct();
-}
 
 //태그 검사후 충돌시 무적시간 잠시 넣어주고 맞을떄마다 
-public class Destructable : MonoBehaviour, IDestructible
+public class Destructable : MonoBehaviour
 {
-    private int _currentHit = 0;
-    public int destructHit = 3;
-    private float HitTime = 0f;
     public Animator _animator;
     public bool isDestructed = false;
     public string[] _damagableTag;
-    public int CoinSpawn = 0;
-    public GameObject[] Effects;
+    public int coinSpawn = 0;
 
-    public void Hit()
+    public CombatComponent combat;
+
+
+
+    private void Awake()
     {
-        //무적시간, 이펙트, 애니메이션
-        HitTime = Time.time;
-        if (_animator != null )
+        foreach (string tag in _damagableTag)
+        {
+            Assert.IsTrue(tag != "");
+        }
+        //파괴 처리
+        combat.Init(transform);
+        combat.OnDead += Destruct;
+        combat.OnDamaged += Hit;
+
+    }
+    private void Hit()
+    {
+        if (_animator != null)
         {
             _animator.SetTrigger("Hit");
         }
-        _currentHit++;
     }
     public void Destruct()
     {
-        ObjectSpawnManager.Instance.SpawnMoney(transform.position + Vector3.up * .4f, CoinSpawn);
+        ObjectSpawnManager.Instance.SpawnMoney(transform.position + Vector3.up * .4f, coinSpawn);
         if (_animator != null)
         {
             _animator.SetTrigger("Dead");
@@ -43,21 +47,10 @@ public class Destructable : MonoBehaviour, IDestructible
         }
         isDestructed = true;
     }
-
-
-
-    private void Awake()
-    {
-        foreach (string tag in _damagableTag)
-        {
-            Assert.IsTrue(tag != "");
-        }
-
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //파괴
-        if (isDestructed)
+        if (combat.IsDead())
         { return; }
         //공격했을때
         //damagableTag는 무조건 IDamageable 보장해야함
@@ -67,24 +60,7 @@ public class Destructable : MonoBehaviour, IDestructible
             if (collision.CompareTag(tag))
             {
                 //힛 처리
-                if (Time.time - HitTime < .1f)
-                {
-                    return;
-                }
-                else
-                {
-                    Hit();
-                }
-                //파괴 처리
-                if (_currentHit >= destructHit)
-                {
-                    Destruct();
-                }
-                //이펙트 스폰
-                foreach (GameObject effect in Effects)
-                {
-                    ObjectSpawnManager.Instance.SpawnBetween(effect ,collision.transform.position, transform.position, 3f);
-                }
+                combat.TakeDamage(collision.transform.position , 1f);
             }
         }
     }
